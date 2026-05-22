53bbc99126be67aa2415e55306aafb900d224521
Abenezer Tekalign <abenezertekalign118@gmail.com>
2026-05-22 22:37:49 +0300

    improve production CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,15 +25,22 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://hams-1-1pbn.onrender.com"
+];
+
 app.use(cors({
- origin:[
-   "http://localhost:5173",
-   "https://hams-1-1pbn.onrender.com"
- ],
- credentials:true,
- methods:["GET","POST","PUT","DELETE","PATCH"],
- allowedHeaders:["Content-Type","Authorization"]
-}))
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
